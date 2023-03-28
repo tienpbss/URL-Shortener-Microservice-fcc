6cbccf8c59d52433493ba2a80e8fdd04c993f3ca
tienpbss <tienphung.cn07@gmail.com>
2023-03-28 14:46:48 +0700

    sua file index.js de chay thanh cong

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const cors = require('cors');
 const mongoose = require('mongoose')
 const urlSchema = require('./url-schema')
 const isUrl = require('is-url')
+const bodyParser = require('body-parser')
 
 const app = express();
 
 
 //connect to atlas
-mongoose.connect(process.env.MONGOOSE_URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGOOSE_URI)
 
-const Url = mongoose.model('Urli', urlSchema)
+
+// dat ten ben trong la urli de sau su dung test xem khi nao thi dung url, khi nao dung urli
+const Url = mongoose.model('Urli', urlSchema) 
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -19,6 +22,9 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.use(express.json())
+// app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -29,9 +35,11 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.post('/api/shorturl', (req, res => {
-
-}))
+app.post('/api/shorturl', (req, res) => {
+  const fullUrl = req.body.url;
+  console.log(req.body, fullUrl)
+  res.send()
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
